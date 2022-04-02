2b9a8b72603fb531c9b63752c0751a14e7740dc7
tj.springboot <2823924648@qq.com>
2022-04-02 06:44:23 +0000

    update src/main/java/com/attaojie/git/testgit.java.

--- src/main/java/com/attaojie/git/testgit.java
+++ src/main/java/com/attaojie/git/testgit.java
@@ -8,5 +8,6 @@ public class testgit {
         System.out.println("Hello git4");
         System.out.println("push git");
         System.out.println("pull git");
+        System.out.println("gitee pull");
     }
 }
