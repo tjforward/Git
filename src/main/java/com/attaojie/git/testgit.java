42fe5a8af1673d9bbfddd31f4d8a0ab3cbf81860
taojie <taojie@qq.com>
2022-04-02 11:06:20 +0800

    secont ideacommit

--- src/main/java/com/attaojie/git/testgit.java
+++ src/main/java/com/attaojie/git/testgit.java
@@ -3,5 +3,6 @@ package com.attaojie.git;
 public class testgit {
     public static void main(String[] args) {
         System.out.println("Hello git");
+        System.out.println("Hello git2");
     }
 }
