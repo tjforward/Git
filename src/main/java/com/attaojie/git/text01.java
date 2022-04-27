f372032747a995874a7420ca600d1be766538917
taojie <taojie@qq.com>
2022-04-27 12:51:10 +0800

    giteefirst

--- src/main/java/com/attaojie/git/text01.java
+++ src/main/java/com/attaojie/git/text01.java
@@ -1,4 +1,14 @@
 package com.attaojie.git;
 
 public class text01 {
+     public static void main(String[] args) {
+        System.out.println("Hello git");
+        System.out.println("Hello git2");
+        System.out.println("Hello git3");
+        System.out.println("Hello git4");
+        System.out.println("push git");
+        System.out.println("pull git");
+        System.out.println("gitee push");
+        System.out.println("gitee pull");
+    }
 }
